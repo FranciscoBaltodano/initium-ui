5caa244ebff60fc8a239dd1035840bd509c1945c
José Francisco Mejía Baltodano <valeadasfran30@gmail.com>
2024-11-29 02:14:35 -0600

    azure api domain

--- src/services/settings.js
+++ src/services/settings.js
@@ -1,2 +1,2 @@
-export const domain = 'http://localhost:8000';
-// export const domain = 'https://sisapi-dev.azurewebsites.net';
+// export const domain = 'http://localhost:8000';
+export const domain = 'https://sisapi-dev.azurewebsites.net';
